1c0f93562ce7901e6193a5c3044a76e937ccae7b
Marcel Wölke <m.woelke@hotmail.de>
2024-03-01 14:26:43 +0100

    small changes to server, test files

--- src/kais_obergeiler_server.py
+++ src/kais_obergeiler_server.py
@@ -9,6 +9,8 @@ PORT = 53
 
 def concatenate_messages(rec_incoming_messages):
     # Initialize an empty bytes object to hold the concatenated message
+    # print(rec_incoming_messages)
+
     concat_message = b""
 
     # Iterate over the bytes object in steps of 64 bytes (63 bytes of message + 1 byte dividing character)
@@ -16,15 +18,15 @@ def concatenate_messages(rec_incoming_messages):
         # Append the first 63 bytes of each chunk to the concat_message
         concat_message += rec_incoming_messages[i:i + 63]
 
-    print("concat message: ", concat_message)
+    if concat_message != b'':
+        print("concat message: ", concat_message)
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST,PORT))
+    print("Server is up and running")
     while True:
-        print("Server is up and running")
-        data = s.recvfrom(1024)
-        data = data[0]
+        data = s.recv(1024)
         # greps the length field
         rec_length = data[12:13]
         # greps the message
@@ -35,12 +37,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         #print("message: ", rec_incoming_messages)
         if len(rec_incoming_messages) > 63:
             #ends the connection & adds the message to the concat message
-            if rec_incoming_messages[-4:] == "ffff":
-                print("Connection Closed")
-                concatenate_messages(rec_incoming_messages[:-4])
+            if rec_incoming_messages[-2:] == b"\xff\xff":
+                concatenate_messages(rec_incoming_messages[:-2])
+                print('ENDE')
             #adds the message to the concat message
             else:
                 concatenate_messages(rec_incoming_messages)
-
-
-
+        else:
+            concatenate_messages(rec_incoming_messages[:-2])
